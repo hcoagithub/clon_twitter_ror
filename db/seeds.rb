139510ea67396d9571354aa6b609c73caa335418
hcoagithub <hectorcoa.14@gmail.com>
2023-06-30 22:47:29 +0000

    db:seed faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+1000.times do |t|
+    Twitter.create(description:Faker::Lorem.paragraphs, user_name:Faker::Twitter.screen_name)
+end
